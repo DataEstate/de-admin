64574e11d07d4cd9c7072849a1b268df235500f6
Rolf Chen <rolf.chen@dataestate.com.au>
2020-01-15 19:40:23 +1000

    flow configurations update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+// @flow
+
 import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import { configureStore } from "./store";
-
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import { App } from "./views";
+import { getDefaultAppStore } from "./helpers";
+
 import { dataEstateTheme } from "./themes/dataEstateTheme";
 
 const appContainer = document.getElementById("root");
